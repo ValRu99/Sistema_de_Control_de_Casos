f4a88dca1c631236caf4461478e2765cff062d9a
ValRu99 <valeriarpenalver@gmail.com>
2023-06-30 23:01:24 -0400

    creación del navbar para el homepage

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bufete</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="main">
+
+     <!--NAVBAR-->
+     <div class="navbar">
+        <div class="icon">
+            <h2 class="logo">LogoBufete</h2>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">NOSOTROS</a></li>
+                <li><a href="#">SERVICIOS</a></li>
+                <li><a href="#">EQUIPO</a></li>
+                <li><a href="#">CONTACTO</a></li>
+            </ul>
+        </div>
+
+    </div>
+        
+    <!-- FIN NAVBAR-->
+
+</div>
+    
+</body>
+</html>
